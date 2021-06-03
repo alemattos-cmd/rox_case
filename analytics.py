3efd47dc3a15f78e5b8c7c08a17b52a8133d64dd
alemattos-cmd <85233149+alemattos-cmd@users.noreply.github.com>
2021-06-02 22:47:53 -0300

    Update analytics.py

--- analytics.py
+++ analytics.py
@@ -16,4 +16,23 @@ with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+da
         while row:
             print (str(row[0]) + " " + str(row[1]))
             row = cursor.fetchone()
-            
+
+
+## Análise de Dados
+
+## 1.	Escreva uma query que retorna a quantidade de linhas na tabela Sales.SalesOrderDetail pelo campo SalesOrderID, 
+##      desde que tenham pelo menos três linhas de detalhes.
+
+## 2.	Escreva uma query que ligue as tabelas Sales.SalesOrderDetail, Sales.SpecialOfferProduct e Production.Product
+##      e retorne os 3 produtos (Name) mais vendidos (pela soma de OrderQty), agrupados pelo número de dias para manufatura (DaysToManufacture).
+
+## 3.	Escreva uma query ligando as tabelas Person.Person, Sales.Customer e Sales.SalesOrderHeader de forma a obter uma lista de nomes de clientes e 
+##      uma contagem de pedidos efetuados.
+
+## 4.	Escreva uma query usando as tabelas Sales.SalesOrderHeader, Sales.SalesOrderDetail e Production.Product, de forma a obter 
+##      a soma total de produtos (OrderQty) por ProductID e OrderDate.
+
+## 5.	Escreva uma query mostrando os campos SalesOrderID, OrderDate e TotalDue da tabela Sales.SalesOrderHeader. 
+##      Obtenha apenas as linhas onde a ordem tenha sido feita durante o mês de setembro/2011 e o total devido esteja acima de 1.000. 
+##      Ordene pelo total devido decrescente.
+
